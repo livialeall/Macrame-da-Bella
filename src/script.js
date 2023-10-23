56bfe453cc3cae6a917d1d9e2d55ca010b7a66e7
Livia Leal <liviabenine@gmail.com>
2023-10-22 23:13:55 -0300

    v2

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 
 const ul = document.querySelector('.lista-imgs')
-
+const imgs = ul.querySelectorAll('img')
 class ImagemHover {
     constructor(){
         this.eventos()
@@ -8,41 +8,52 @@ class ImagemHover {
 
     eventos(){
 
-        ul.addEventListener('mouseover',e=>{
-         const el = e.target
-         console.log(el)
-         const id = el.id
-         const tag = el.tagName.toLowerCase()
-            if(tag === 'img') {
-                this.handleMouseOver(id,el)
-
-            }
-            else{
-                
-            }
-
-        })
-
+        for(let img of imgs) {
+            img.addEventListener('mouseenter',e=>{
+                const el = e.target
+                console.log(el)
+                const id = el.id
+                this.handleMouseEnter(id,el)
+               })
+            
+            img.addEventListener('mouseout',e=>{
+                const el = e.target
+                const id = el.id
+                if(id ==='hover'){
+                    this.handleMouseOut(el)
+                }
+            })
+            
+        }
+        
         ul.addEventListener('click',e=>{
             const el = e.target
             const imgAnterior = el.previousElementSibling
             const id = imgAnterior.id
             if(id !== ""){
                 this.divFoto(id)
+                this.divParagrafo(id)
             }
         })
     }
 
 
-        divFoto(id){
+       async divFoto(id){
             this.removerDiv()
             const div =  document.createElement('div')
             const img = `<img src="img-categoria/${id}.jpg" alt="${id}">`
             div.innerHTML = img
             div.classList.add('div-img')
             ul.insertAdjacentElement('afterend',div)
+
+            const p =  document.createElement('p')
+            const texto = await fetch(`./textos/${id}.txt`)
+            const responseTexto = await texto.text()
+            p.innerHTML = responseTexto
+            p.classList.add('div-descricao')
+            div.insertAdjacentElement('beforeend',p)
+
          }
-    
         
          removerDiv() {
             const divsRemove = document.querySelectorAll('.div-img')
@@ -52,19 +63,77 @@ class ImagemHover {
          }
 
 
-    handleMouseOver(id,el){
+        handleMouseEnter(id,el){
         const imgsHover = ul.querySelectorAll('#hover')
         for(let img of imgsHover ) {
             if(img.classList.contains(id)){
-                img.classList.toggle('hidden')
+                img.classList.remove('hidden')
                 el.classList.add('hidden')
-                
             }
         }
+    }
 
+    handleMouseOut(el){
+        const imgsOriginal = ul.querySelectorAll('.original')
+        for(let img of imgsOriginal ) {
+            img.classList.remove('hidden')
+            el.classList.add('hidden')
+            
+        }
     }
 
 }
 
-    
 const imgHover = new ImagemHover()
+
+
+
+const divCarrossel = document.querySelector('#carousel')
+const imagens = divCarrossel.querySelectorAll('img')
+let currentIndex = 0
+
+class Carrossel {
+    constructor(){
+        this.eventos()
+    }
+    eventos(){
+        divCarrossel.addEventListener('click',e=>{
+            const el = e.target
+            const id = el.id
+            if(id === 'previous'){
+                this.handleClickPrevious()
+              
+            }
+
+            if(id === 'next') {
+                this.handleClickNext()
+                
+            }
+        })
+    }
+
+
+    handleClickNext(){
+        imagens[currentIndex].classList.remove('block')
+        imagens[currentIndex].classList.add('hidden')
+        currentIndex = (currentIndex + 1) % imagens.length /* se eu chego no ultimo, a conta retorna zero e eu volto para a primeira foto */
+        imagens[currentIndex].classList.remove('hidden')
+    }
+
+    handleClickPrevious(){
+        
+        imagens[currentIndex].classList.remove('block')
+        imagens[currentIndex].classList.add('hidden')
+        if(currentIndex == 0) {
+            currentIndex = imagens.length -1
+        }else{
+            currentIndex = (currentIndex - 1)
+        }
+        console.log(currentIndex)
+        imagens[currentIndex].classList.remove('hidden')
+    }
+}
+
+const carrossel = new Carrossel
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       colors:{
         areia:{
           100:'#FBF9F5',
+          200:'#F4F1EC',
           500:'#D8C7A9',
           600:'#CCB38B',
 
@@ -13,20 +14,21 @@ module.exports = {
         mostarda:{
           300:'#E9A84A',
           400:'#DF9C3B',
+          500:'#D18D2A',
+          600:'#C8821B',
+          700:'#BF7509',
           transparent:'#FFC167',
         },
         azul:{
           500:'#3B6FAB',
-          700:'#164881',
+          700:'#0B3B72',
         }
       },
-
-      fontFamily:{
-        texto:['Roboto Slab', 'serif'],
-        titulo:['DM Serif Display', 'serif'],
-        destque:['Bilbo Swash Caps', 'cursive']
-      },
     },
+
+    fontFamily:{
+      'sans':['Roboto']
+    }
 
     
   },
